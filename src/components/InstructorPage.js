cfe6136cec5c3a4ad1dbf53d3cb66a65cda9fe09
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-05-24 15:17:16 -0400

    template create class

---
+++ src/components/InstructorPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+const InstructorPage = () => {
+
+    const { push } = useHistory()
+
+    const createButtonHandler = () => {
+        push('/')
+    }
+    
+    return (
+        <div>
+            <h2> **Show a list of all classes here with edit and delete option possibly??**</h2>
+            <button onClick={createButtonHandler}>Create a Class</button>
+        </div>
+    )
+}
+
+export default InstructorPage
